8a78adf1a52f61dd86be2da55ecd5a2326b772b7
Serhii1727 <kravchenko087@ukr.net>
2023-12-24 22:00:26 -0500

    add Loader

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,16 +1,18 @@
-import { PureComponent } from 'react';
+import { Component } from 'react';
+import { BallTriangle } from 'react-loader-spinner';
 import fetchImage from 'components/api/api';
 import ImageGalleryItem from 'components/ImageGalleryItem';
 import Button from 'components/Button';
 import Modal from 'components/Modal';
 import css from './ImageGallery.module.css';
 
-export default class ImageGallery extends PureComponent {
+export default class ImageGallery extends Component {
   state = {
     page: 1,
     images: [],
     showModal: false,
     modalData: null,
+    loading: false,
   };
 
   componentDidUpdate(prevProp, prevState) {
@@ -19,40 +21,46 @@ export default class ImageGallery extends PureComponent {
 
     if (prevProp.searchQuery !== this.props.searchQuery) {
       this.setState({ page: 1, images: [] });
+      this.setState({ loading: true });
 
-      fetchImage(searchQuery, page).then(data => {
-        const { hits } = data;
+      fetchImage(searchQuery, page)
+        .then(data => {
+          const { hits } = data;
 
-        const images = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
-          return { id, webformatURL, largeImageURL, tags };
-        });
+          const images = hits.map(
+            ({ id, webformatURL, largeImageURL, tags }) => {
+              return { id, webformatURL, largeImageURL, tags };
+            }
+          );
 
-        this.setState({ images });
-        this.setState(prevState => ({
-          page: prevState.page + 1,
-        }));
-      });
+          this.setState({ images });
+          this.setState(prevState => ({
+            page: prevState.page + 1,
+          }));
+        })
+        .finally(this.setState({ loading: false }));
     }
   }
 
   loadMore = () => {
     const { searchQuery } = this.props;
     const { page } = this.state;
+    this.setState({ loading: true });
 
-    fetchImage(searchQuery, page).then(data => {
-      const { hits } = data;
+    fetchImage(searchQuery, page)
+      .then(data => {
+        const { hits } = data;
 
-      const images = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
-        return { id, webformatURL, largeImageURL, tags };
-      });
+        const images = hits.map(({ id, webformatURL, largeImageURL, tags }) => {
+          return { id, webformatURL, largeImageURL, tags };
+        });
 
-      console.log(images);
-
-      this.setState(prevState => ({
-        page: prevState.page + 1,
-        images: [...prevState.images, ...images],
-      }));
-    });
+        this.setState(prevState => ({
+          page: prevState.page + 1,
+          images: [...prevState.images, ...images],
+        }));
+      })
+      .finally(this.setState({ loading: false }));
   };
 
   closeModal = () => {
@@ -65,10 +73,24 @@ export default class ImageGallery extends PureComponent {
   };
 
   render() {
-    const { images, showModal, modalData } = this.state;
+    const { images, showModal, modalData, loading } = this.state;
 
     return (
       <>
+        {loading && (
+          <div className={css.loader}>
+            <BallTriangle
+              height={100}
+              width={100}
+              radius={5}
+              color="blue"
+              ariaLabel="ball-triangle-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              visible={true}
+            />
+          </div>
+        )}
         <ul className={css.imageGallery}>
           {images.map(({ id, webformatURL, largeImageURL, tags }) => (
             <ImageGalleryItem
